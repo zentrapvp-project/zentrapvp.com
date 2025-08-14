893c039363b1499f7b7d3ee4d636f011109ce93b
ThisIsTheMatin <134326446+ThisIsTheMatin@users.noreply.github.com>
2025-08-15 01:05:42 +0330

    Fixing Home Page

--- app/page.js
+++ app/page.js
@@ -8,16 +8,6 @@ export default function Home() {
   return (
     <>
       <div className={HomepageStyles.mainSection}>
-
-        
-        <Image className={`${HomepageStyles.topLeft}`} width={150} height={150} alt="Yo" src={`https://khlahvgbqpyjfmwrjayp.supabase.co/storage/v1/object/public/cortik/logo/120px-Enchanted_Golden_Helmet_(item)%201.png`} />
-        <Image className={`${HomepageStyles.topLeft} ${HomepageStyles.glow}`} width={150} height={150} alt="Yo" src={`https://khlahvgbqpyjfmwrjayp.supabase.co/storage/v1/object/public/cortik/logo/120px-Enchanted_Golden_Helmet_(item)%201.png`} />
-        <Image className={`${HomepageStyles.topRight}`} width={150} height={150} alt="Yo" src={`https://khlahvgbqpyjfmwrjayp.supabase.co/storage/v1/object/public/cortik/logo/Creeper_Head_(S)_JE1%201.png`} />
-        <Image className={`${HomepageStyles.topRight} ${HomepageStyles.glow}`} width={150} height={150} alt="Yo" src={`https://khlahvgbqpyjfmwrjayp.supabase.co/storage/v1/object/public/cortik/logo/Creeper_Head_(S)_JE1%201.png`} />
-        <Image className={`${HomepageStyles.bottomLeft}`} width={150} height={150} alt="Yo" src={`https://khlahvgbqpyjfmwrjayp.supabase.co/storage/v1/object/public/cortik/logo/Enchanted_Diamond_Sword%201.png`} />
-        <Image className={`${HomepageStyles.bottomLeft} ${HomepageStyles.glow}`} width={150} height={150} alt="Yo" src={`https://khlahvgbqpyjfmwrjayp.supabase.co/storage/v1/object/public/cortik/logo/Enchanted_Diamond_Sword%201.png`} />
-        <Image className={`${HomepageStyles.bottomRight}`} width={150} height={150} alt="Yo" src={`https://khlahvgbqpyjfmwrjayp.supabase.co/storage/v1/object/public/cortik/logo/PotionOfNightVisionNew%201.png`} />
-        <Image className={`${HomepageStyles.bottomRight} ${HomepageStyles.glow}`} width={150} height={150} alt="Yo" src={`https://khlahvgbqpyjfmwrjayp.supabase.co/storage/v1/object/public/cortik/logo/PotionOfNightVisionNew%201.png`} />
         <div className={HomepageStyles.mainContent}>
             <h1>ZentraPvP</h1>
             <p>The first Iranian Minecraft server focused on Compitition and PvP</p>
@@ -25,6 +15,14 @@ export default function Home() {
             <div className={HomepageStyles.actionsContainer}>
             <Button type={"alt"} >play.zentrapvp.com</Button>
             <Button>Connect</Button>
+            <Image className={`${HomepageStyles.topLeft}`} width={150} height={150} alt="Yo" src={`https://khlahvgbqpyjfmwrjayp.supabase.co/storage/v1/object/public/cortik/logo/120px-Enchanted_Golden_Helmet_(item)%201.png`} />
+            <Image className={`${HomepageStyles.topLeft} ${HomepageStyles.glow}`} width={150} height={150} alt="Yo" src={`https://khlahvgbqpyjfmwrjayp.supabase.co/storage/v1/object/public/cortik/logo/120px-Enchanted_Golden_Helmet_(item)%201.png`} />
+            <Image className={`${HomepageStyles.topRight}`} width={150} height={150} alt="Yo" src={`https://khlahvgbqpyjfmwrjayp.supabase.co/storage/v1/object/public/cortik/logo/Creeper_Head_(S)_JE1%201.png`} />
+            <Image className={`${HomepageStyles.topRight} ${HomepageStyles.glow}`} width={150} height={150} alt="Yo" src={`https://khlahvgbqpyjfmwrjayp.supabase.co/storage/v1/object/public/cortik/logo/Creeper_Head_(S)_JE1%201.png`} />
+            <Image className={`${HomepageStyles.bottomLeft}`} width={150} height={150} alt="Yo" src={`https://khlahvgbqpyjfmwrjayp.supabase.co/storage/v1/object/public/cortik/logo/Enchanted_Diamond_Sword%201.png`} />
+            <Image className={`${HomepageStyles.bottomLeft} ${HomepageStyles.glow}`} width={150} height={150} alt="Yo" src={`https://khlahvgbqpyjfmwrjayp.supabase.co/storage/v1/object/public/cortik/logo/Enchanted_Diamond_Sword%201.png`} />
+            <Image className={`${HomepageStyles.bottomRight}`} width={150} height={150} alt="Yo" src={`https://khlahvgbqpyjfmwrjayp.supabase.co/storage/v1/object/public/cortik/logo/PotionOfNightVisionNew%201.png`} />
+            <Image className={`${HomepageStyles.bottomRight} ${HomepageStyles.glow}`} width={150} height={150} alt="Yo" src={`https://khlahvgbqpyjfmwrjayp.supabase.co/storage/v1/object/public/cortik/logo/PotionOfNightVisionNew%201.png`} />
             </div>
         </div>
 
